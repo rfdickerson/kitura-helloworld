964aee14b590d4c7ac820455ec49ca24a9ad1282
Patrick Bohrer <pbohrer@us.ibm.com>
2016-11-07 10:32:59 -0600

    update procfile too

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "tinyswift",
+    name: "kitura-helloworld",
 	dependencies: [
 		.Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1, minor: 1),
 		.Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 1)
